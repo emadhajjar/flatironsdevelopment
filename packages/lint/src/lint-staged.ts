53e6e9d156469385c7a98911aec173b1381f6707
Emad Hajjar <emadaldeen.hajjar@gmail.com>
2025-03-09 17:48:29 -0300

    fix: update web package

--- packages/lint/src/lint-staged.ts
+++ packages/lint/src/lint-staged.ts
@@ -3,5 +3,5 @@ import type { Configuration } from 'lint-staged';
 export const lintStagedConfig: Configuration = {
   '**/*.(md|json)': ['yarn prettier --write'],
   '**/*.(ts|tsx)': ['yarn web check-types'],
-  '**/*.{js,ts,jsx,tsx}': ['yarn format'],
+  '**/*.{js,ts,jsx,tsx}': ['yarn format --quiet'],
 };
